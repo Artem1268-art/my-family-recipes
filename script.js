b5b2e6a4e38e5c0915cd3c8ea0b4e21a22542aaf
Artem1268-art <ha251004oaa@gmail.com>
2026-09-19 22:11:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     function resetUploadStatus() {
         fileUploadLabel.classList.remove('success');
-        uploadStatusText.textContent = '📸 Загрузить сочное фото';
+        uploadStatusText.textContent = '📸 Загрузить фото';
         imagePreviewContainer.innerHTML = '';
         temporaryImageBase64 = "";
     }
@@ -171,7 +171,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!toast) {
             toast = document.createElement('div');
             toast.className = 'toast-success';
-            toast.innerHTML = '✅ Добавлено! 🍋';
+            toast.innerHTML = '✅ Добавлено!';
             document.body.appendChild(toast);
         }
         setTimeout(() => { toast.classList.add('show'); }, 30);
