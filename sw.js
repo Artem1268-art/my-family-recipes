51cc7b1132b6fb0b054b18b6a7be2b9860e2e7fe
Artem1268-art <ha251004oaa@gmail.com>
2026-09-19 19:12:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
-const CACHE_NAME = 'lime-recipes-v3';
+const CACHE_NAME = 'lime-recipes-v4';
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const APP_FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  // Пропускаем все фоновые проверки, чтобы телефон мгновенно одобрял безопасность сайта
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
 });
-
