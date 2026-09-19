d437a8f1fbdd8938bc4d5028ba0e2141ff6194cd
Artem1268-art <ha251004oaa@gmail.com>
2026-09-19 18:57:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'lime-recipes-v1';
-const ASSETS = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.json'
-];
+const CACHE_NAME = 'lime-recipes-v2';
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'script.js',
+        'manifest.json'
+      ]).catch(err => console.log('Встроенный пропуск кэша:', err));
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
 });
+
